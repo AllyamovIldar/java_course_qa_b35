7740f547baef861d18ba15de25eec356ccfefb51
AllyamovIldar <RBlade839494@gmail.com>
2022-10-17 23:13:25 +0400

    Реализована программа по вычислению расстояния между двумя точками на координатной плоскости
    Реализация п.1-3 (из 4)

---
+++ sandbox/src/main/java/lesson/one/task2/MainClassForSecondProgram.java
@@ -0,0 +1,17 @@
+package lesson.one.task2;
+
+public class MainClassForSecondProgram {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.x = 2.0;
+        p1.y = 3.0;
+        Point p2 = new Point();
+        p2.x = 5.0;
+        p2.y = 7.0;
+        System.out.println("Расстояние между точкой №1 и точкой №2 = " + distance(p1, p2));
+    }
+    public static double distance(Point p1, Point p2) {
+        double sqrtValue = Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+        return (sqrtValue);
+    }
+}
---
+++ sandbox/src/main/java/lesson/one/task2/Point.java
@@ -0,0 +1,6 @@
+package lesson.one.task2;
+
+public class Point {
+    public double x;
+    public double y;
+}
