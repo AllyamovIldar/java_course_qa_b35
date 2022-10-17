8d8e7ec5f9acf0c0a3b37be7aec29eb0c1baaa6c
AllyamovIldar <RBlade839494@gmail.com>
2022-10-18 00:34:51 +0400

    Добавлено вычисление расстояния через метод distance2
    п.4 задания №2

--- sandbox/src/main/java/lesson/one/task2/MainClassForSecondProgram.java
+++ sandbox/src/main/java/lesson/one/task2/MainClassForSecondProgram.java
@@ -5,11 +5,16 @@ public class MainClassForSecondProgram {
         Point p1 = new Point();
         p1.x = 2.0;
         p1.y = 3.0;
+        System.out.println("Точка №1 имеет координаты x,y = " + p1.x + "," + p1.y);
         Point p2 = new Point();
         p2.x = 5.0;
         p2.y = 7.0;
-        System.out.println("Расстояние между точкой №1 и точкой №2 = " + distance(p1, p2));
+        System.out.println("Точка №2 имеет координаты x,y = " + p2.x + "," + p2.y);
+
+        System.out.println("Вычисление через функцию distance. Расстояние между точкой №1 и точкой №2 = " + distance(p1, p2));
+        System.out.println("Вычисление через метод distance2. Расстояние между точкой №1 и точкой №2 = " + p1.distance2(p2));
     }
+
     public static double distance(Point p1, Point p2) {
         return (Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2)));
     }
