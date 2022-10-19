369ba7b488a7cc52e32f59c1a72512c2fb22eb4a
AllyamovIldar <RBlade839494@gmail.com>
2022-10-20 02:07:55 +0400

    Добавлены различные тесты для Point и distance2

--- sandbox/src/test/java/lesson/one/task2/DistanceTests.java
+++ sandbox/src/test/java/lesson/one/task2/DistanceTests.java
@@ -5,13 +5,49 @@ import org.testng.annotations.Test;
 
 public class DistanceTests {
     @Test
-    public void testAssertBasicValuePoint(){
+    public void testAssertEquals() {
         Point pointVal1 = new Point();
-        pointVal1.x = 2;
-        pointVal1.y = 3;
+        pointVal1.x = 3;
+        pointVal1.y = 8;
+        Point pointVal2 = new Point();
+        pointVal2.x = 7;
+        pointVal2.y = 2;
+        Assert.assertEquals(pointVal2.distance2(pointVal1), 7.211102550927978);
+    }
+
+    @Test
+    public void testAssertNotEquals() {
+        Point pointVal1 = new Point();
+        pointVal1.x = 3;
+        pointVal1.y = 5;
         Point pointVal2 = new Point();
         pointVal2.x = 5;
-        pointVal2.y = 7;
-        Assert.assertEquals(pointVal2.distance2(pointVal1), 5.0);
+        pointVal2.y = 8;
+        Assert.assertNotEquals(pointVal2.distance2(pointVal1), 7);
+    }
+
+    @Test
+    public void testDoubleX() {
+        Point pointVal1 = new Point();
+        pointVal1.x = 6;
+        Assert.assertEquals(pointVal1.x, 6.0);
+    }
+
+    @Test
+    public void testDoubleY() {
+        Point pointVal1 = new Point();
+        pointVal1.y = 9;
+        Assert.assertEquals(pointVal1.y, 9.0);
+    }
+
+    @Test
+    public void testDistanceGreaterThanZeroOrEqualsZero() {
+        Point pointVal1 = new Point();
+        pointVal1.x = 3;
+        pointVal1.y = 5;
+        Point pointVal2 = new Point();
+        pointVal2.x = 5;
+        pointVal2.y = 8;
+        Assert.assertTrue(pointVal2.distance2(pointVal1) >= 0.0);
     }
 }
