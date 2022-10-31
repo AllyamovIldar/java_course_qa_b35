70d091d302d3a75907c9bcd3156a90643eb425ff
AllyamovIldar <RBlade839494@gmail.com>
2022-11-01 01:04:57 +0400

    Добавлена проверка на наличие контактов перед удалением

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/AddressDeletionTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/AddressDeletionTests.java
@@ -6,6 +6,9 @@ import org.testng.annotations.Test;
 public class AddressDeletionTests extends TestBase {
     @Test
     public void testAddressDeletion() {
+        if (!app.getContactHelper().isThereAnAddress()) {
+            app.getContactHelper().createAddress(new AddressData("FirstNameExample", "MiddleNameExample", "LastNameExample", "TestCompany", "TestAddress", "84951112233", "89995554433", "88009876543", "testmail1@mail.com", "testmail2@email.com", "testmail3@mailtest.com"));
+        }
         app.getContactHelper().selectAddress();
         app.getContactHelper().initAddressDeletion();
         app.getNavigationHelper().gotoHomePage();
