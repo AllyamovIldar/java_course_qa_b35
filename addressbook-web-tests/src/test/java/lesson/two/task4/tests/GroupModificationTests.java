eafd682ad3e17c6c562336f1b66ff68580d3f002
AllyamovIldar <RBlade839494@gmail.com>
2022-11-11 02:35:34 +0400

    Добавлена проверка на подсчет элементов при модификации групп

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupModificationTests.java
@@ -1,12 +1,14 @@
 package lesson.two.task4.tests;
 
 import lesson.two.task4.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("some test group from recorder", "some header from recorder (test existing)", "some footer from recorder"));
         }
@@ -15,5 +17,7 @@ public class GroupModificationTests extends TestBase {
         app.getGroupHelper().fillGroupForm(new GroupData("Modified group name", "some header from recorder (test existing)", "Modified group footer"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
     }
 }
