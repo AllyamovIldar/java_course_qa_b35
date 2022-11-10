bd3f1d4b391bbc38986329ed2821884f0fee78b5
AllyamovIldar <RBlade839494@gmail.com>
2022-11-11 02:42:19 +0400

    Добавлена проверка на подсчет элементов при модификации контактов

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/AddressModificationTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/AddressModificationTests.java
@@ -2,11 +2,13 @@ package lesson.two.task4.tests;
 
 import lesson.two.task4.model.AddressData;
 import lesson.two.task4.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AddressModificationTests extends TestBase {
     @Test
     public void testAddressModification() {
+        int before = app.getContactHelper().getContactCount();
         if (!app.getContactHelper().isThereAnAddress()) {
             app.getContactHelper().createAddress(new AddressData("FirstNameExample", "MiddleNameExample", "LastNameExample", "TestCompany", "TestAddress", "84951112233", "89995554433", "88009876543", "testmail1@mail.com", "testmail2@email.com", "testmail3@mailtest.com"));
         }
@@ -15,5 +17,7 @@ public class AddressModificationTests extends TestBase {
         app.getContactHelper().fillAddressForm(new AddressData("FirstNameExample1", "MiddleNameExample1", "LastNameExample1", "TestCompany1", "TestAddress1", "84951112231", "89995554431", "88009876541", "test1mail1@mail.com", "test1mail2@email.com", "test1mail3@mailtest.com"));
         app.getContactHelper().submitAddressModification();
         app.getContactHelper().returnToHomePage();
+        int after = app.getContactHelper().getContactCount();
+        Assert.assertEquals(after, before);
     }
 }
