3e48a68b4dbee70dfc5386c4a8c43f4bc9618a57
AllyamovIldar <RBlade839494@gmail.com>
2022-11-11 02:34:10 +0400

    Добавлена проверка на подсчет элементов при удалении групп

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupDeletionTests.java
@@ -1,17 +1,21 @@
 package lesson.two.task4.tests;
 
 import lesson.two.task4.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("some test group from recorder", "some header from recorder (test existing)", "some footer from recorder"));
         }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupDeletion();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before - 1);
     }
 }
