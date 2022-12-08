55c8a7fd10383d4eb7879209c0a6a9647999f276
AllyamovIldar <RBlade839494@gmail.com>
2022-12-08 16:19:58 +0400

    Исправление ошибки с before для контактов

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/AddressDeletionTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/AddressDeletionTests.java
@@ -7,10 +7,10 @@ import org.testng.annotations.Test;
 public class AddressDeletionTests extends TestBase {
     @Test
     public void testAddressDeletion() {
-        int before = app.getContactHelper().getContactCount();
         if (!app.getContactHelper().isThereAnAddress()) {
             app.getContactHelper().createAddress(new AddressData("FirstNameExample", "MiddleNameExample", "LastNameExample", "TestCompany", "TestAddress", "84951112233", "89995554433", "88009876543", "testmail1@mail.com", "testmail2@email.com", "testmail3@mailtest.com"));
         }
+        int before = app.getContactHelper().getContactCount();
         app.getContactHelper().selectAddress(before - 1);
         app.getContactHelper().initAddressDeletion();
         app.getNavigationHelper().gotoHomePage();
--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/AddressModificationTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/AddressModificationTests.java
@@ -8,10 +8,10 @@ import org.testng.annotations.Test;
 public class AddressModificationTests extends TestBase {
     @Test
     public void testAddressModification() {
-        int before = app.getContactHelper().getContactCount();
         if (!app.getContactHelper().isThereAnAddress()) {
             app.getContactHelper().createAddress(new AddressData("FirstNameExample", "MiddleNameExample", "LastNameExample", "TestCompany", "TestAddress", "84951112233", "89995554433", "88009876543", "testmail1@mail.com", "testmail2@email.com", "testmail3@mailtest.com"));
         }
+        int before = app.getContactHelper().getContactCount();
         app.getContactHelper().selectAddress(before - 1);
         app.getContactHelper().initAddressModification();
         app.getContactHelper().fillAddressForm(new AddressData("FirstNameExample1", "MiddleNameExample1", "LastNameExample1", "TestCompany1", "TestAddress1", "84951112231", "89995554431", "88009876541", "test1mail1@mail.com", "test1mail2@email.com", "test1mail3@mailtest.com"));
