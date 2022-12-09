1cb333d17ec1e55a0fed982be06dfc6a49cf76a6
AllyamovIldar <RBlade839494@gmail.com>
2022-12-09 14:38:38 +0400

    Сортировка списка для модификации групп
    4.10

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupModificationTests.java
@@ -4,9 +4,8 @@ import lesson.two.task4.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 public class GroupModificationTests extends TestBase {
     @Test
@@ -27,6 +26,9 @@ public class GroupModificationTests extends TestBase {
 
         before.remove(before.size() - 1);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
