3810d5fbad56a7fc01d32ab95f83ff6de06ca376
AllyamovIldar <RBlade839494@gmail.com>
2022-12-14 17:21:35 +0400

    Рефакторинг по лекции 5.5
    Удаление ненужных методов, импортов и пр.

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupDeletionTests.java
@@ -5,14 +5,13 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().groupPage();
-        if (app.group().list().size() == 0) {
+        if (app.group().all().size() == 0) {
             app.group().create(new GroupData().withName("some test group from recorder").withHeader("some header from recorder (test existing)").withFooter("some footer from recorder"));
         }
     }
