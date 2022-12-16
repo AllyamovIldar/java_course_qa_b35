5005c47f2ab3fd01cb768d7828c10315d2c64656
AllyamovIldar <RBlade839494@gmail.com>
2022-12-16 20:08:46 +0400

    Параметризация создания групп через DataProvider
    6.4

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupAndContactCreationTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupAndContactCreationTests.java
@@ -4,20 +4,32 @@ import lesson.two.task4.model.ContactData;
 import lesson.two.task4.model.Contacts;
 import lesson.two.task4.model.GroupData;
 import lesson.two.task4.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupAndContactCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
+        list.add(new Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("another test group from recorder").withHeader("some header from recorder (test existing)").withFooter("some footer from recorder");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
