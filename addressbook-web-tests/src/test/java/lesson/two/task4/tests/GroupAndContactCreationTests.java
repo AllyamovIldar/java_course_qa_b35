8d0f041a948c49d7de46f1dc02082d9903f91214
AllyamovIldar <RBlade839494@gmail.com>
2022-12-17 19:39:47 +0400

    Добавление json генератора тестовых данных для создания групп через Gson
    6.7

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupAndContactCreationTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/GroupAndContactCreationTests.java
@@ -1,5 +1,7 @@
 package lesson.two.task4.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import lesson.two.task4.model.ContactData;
 import lesson.two.task4.model.Contacts;
@@ -22,7 +24,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupAndContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
+    public Iterator<Object[]> validGroupsFromXml() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
         String xml = "";
         String line = reader.readLine();
@@ -37,7 +39,20 @@ public class GroupAndContactCreationTests extends TestBase {
         return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider = "validGroups")
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+    @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
