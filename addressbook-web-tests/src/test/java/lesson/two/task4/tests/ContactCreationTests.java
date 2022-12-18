cd28683e7b78c50761f5a6583a5728b3ebff026f
AllyamovIldar <RBlade839494@gmail.com>
2022-12-19 00:21:48 +0400

    Разделение классов по тестам создания групп и контактов

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/ContactCreationTests.java
@@ -2,11 +2,8 @@ package lesson.two.task4.tests;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.thoughtworks.xstream.XStream;
 import lesson.two.task4.model.ContactData;
 import lesson.two.task4.model.Contacts;
-import lesson.two.task4.model.GroupData;
-import lesson.two.task4.model.Groups;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -21,50 +18,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class GroupAndContactCreationTests extends TestBase {
-
-    @DataProvider
-    public Iterator<Object[]> validGroupsFromXml() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
-            String xml = "";
-            String line = reader.readLine();
-            while (line != null) {
-                xml += line;
-                line = reader.readLine();
-            }
-            XStream xstream = new XStream();
-            xstream.processAnnotations(GroupData.class);
-            xstream.allowTypes(new Class[]{GroupData.class});
-            List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
-            return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
-        }
-    }
-
-    @DataProvider
-    public Iterator<Object[]> validGroupsFromJson() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")))) {
-            String json = "";
-            String line = reader.readLine();
-            while (line != null) {
-                json += line;
-                line = reader.readLine();
-            }
-            Gson gson = new Gson();
-            List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
-            }.getType());
-            return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
-        }
-    }
-
-    @Test(dataProvider = "validGroupsFromJson")
-    public void testGroupCreation(GroupData group) throws Exception {
-        app.goTo().groupPage();
-        Groups before = app.group().all();
-        app.group().create(group);
-        assertThat(app.group().count(), equalTo(before.size() + 1));
-        Groups after = app.group().all();
-        assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-    }
+public class ContactCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validContactsFromJson() throws IOException {
