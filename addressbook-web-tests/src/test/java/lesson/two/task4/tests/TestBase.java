6815d137be2e9926022f007b459fc094390458ad
AllyamovIldar <RBlade839494@gmail.com>
2022-12-19 15:02:35 +0400

    Добавлено протоколирование (лог-файлы)
    6.11

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/TestBase.java
@@ -2,11 +2,19 @@ package lesson.two.task4.tests;
 
 import lesson.two.task4.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     protected static final ApplicationManager app = new ApplicationManager(Browser.CHROME);
 
     @BeforeSuite(alwaysRun = true)
@@ -19,4 +27,13 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod(alwaysRun = true)
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + "with parameters:" + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
 }
