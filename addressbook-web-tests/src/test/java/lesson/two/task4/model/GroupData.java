1b43360c543e40848510ca86d03e9944f4c46ff8
AllyamovIldar <RBlade839494@gmail.com>
2022-12-21 15:30:35 +0400

    Получение данных из БД через ORM
    7.2

--- addressbook-web-tests/src/test/java/lesson/two/task4/model/GroupData.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/model/GroupData.java
@@ -3,18 +3,32 @@ package lesson.two.task4.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
     @XStreamOmitField
+    @Id
+    @Column(name = "group_id")
     private int id = Integer.MAX_VALUE;
     @Expose
+    @Column(name = "group_name")
     private String name;
     @Expose
+    @Column(name = "group_header")
+    @Type(type = "text")
     private String header;
     @Expose
+    @Column(name = "group_footer")
+    @Type(type = "text")
     private String footer;
 
     public int getId() {
@@ -55,10 +69,7 @@ public class GroupData {
 
     @Override
     public String toString() {
-        return "GroupData{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
+        return "GroupData{" + "id='" + id + '\'' + ", name='" + name + '\'' + '}';
     }
 
     @Override
