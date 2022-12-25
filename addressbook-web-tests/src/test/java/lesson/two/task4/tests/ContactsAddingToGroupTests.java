1ba4584a5da1f9b4afa6060ebb60a82efdb46d36
AllyamovIldar <RBlade839494@gmail.com>
2022-12-25 23:48:02 +0400

    Изменение теста под добавление одного контакта в группу + доработка предусловия
    Предусловие: выбирается тот контакт, который в группу не добавлен. А если таких контактов нет, то создается новый.
    
    Исправление задания 16

--- addressbook-web-tests/src/test/java/lesson/two/task4/tests/ContactsAddingToGroupTests.java
+++ addressbook-web-tests/src/test/java/lesson/two/task4/tests/ContactsAddingToGroupTests.java
@@ -9,19 +9,31 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ContactsAddingToGroupTests extends TestBase {
-
     @BeforeMethod
     public void ensurePrecondition() {
         app.contact().returnToHome();
         Groups groups = app.db().groups();
+        Contacts contacts = app.db().contacts();
         String groupName = "test_group_with_adding_contact";
         if (groups.size() == 0 || getFalseGroupExists(groups, groupName)) {
             app.goTo().groupPage();
             app.group().create(new GroupData().withName(groupName));
         }
-        if (getFalseContactWithoutGroupExists(app.db().contacts(), groupName)) {
-            app.contact().create(new ContactData().withFirstName("FirstName_AddingToGroup").withLastName("LastName_AddingToGroup").withAddress("Address_AddingToGroup").withEmail("email_addingtogroup@gmail.com").withHomePhone("89995554488"));
+        if (contacts.size() == 0) {
+            contactCreateTest();
+        } else {
+            int countWithoutLinks = 0;
+            for (ContactData contact : contacts) {
+                boolean contactWithoutLinkedGroups = contact.getGroups().size() == 0;
+                if (contactWithoutLinkedGroups) {
+                    countWithoutLinks++;
+                }
+            }
+            if (countWithoutLinks == 0) {
+                contactCreateTest();
+            }
         }
+
     }
 
     @Test
@@ -33,25 +45,17 @@ public class ContactsAddingToGroupTests extends TestBase {
         GroupData groupData = getGroupByName(groups, groupName);
         for (ContactData contact : contacts) {
             Groups contactGroups = contact.getGroups();
-            if (getFalseGroupExists(contactGroups, groupName)) {
+            boolean contactSizeLessOne = contact.getGroups().size() < 1;
+            if (getFalseGroupExists(contactGroups, groupName) && contactSizeLessOne) {
                 assert groupData != null;
                 app.contact().addContactToGroup(contact, groupData);
                 app.contact().returnToHome();
-            }
-            assert groupData != null;
-            GroupData group = app.db().groupById(groupData.getId());
-            ContactData contactWithGroup = app.db().contactsById(contact.getId());
-            Assert.assertTrue(contactWithGroup.getGroups().contains(group) && group.getContacts().contains(contactWithGroup));
-        }
-    }
-
-    private boolean getFalseContactWithoutGroupExists(Contacts contacts, String name) {
-        for (ContactData contact : contacts) {
-            if (getFalseGroupExists(contact.getGroups(), name)) {
-                return false;
+                GroupData group = app.db().groupById(groupData.getId());
+                ContactData contactWithGroup = app.db().contactsById(contact.getId());
+                Assert.assertTrue(contactWithGroup.getGroups().contains(group) && group.getContacts().contains(contactWithGroup));
+                break;
             }
         }
-        return true;
     }
 
     private GroupData getGroupByName(Groups groups, String name) {
@@ -70,5 +74,9 @@ public class ContactsAddingToGroupTests extends TestBase {
             }
         }
         return true;
+    }
+
+    private static void contactCreateTest() {
+        app.contact().create(new ContactData().withFirstName("FirstName_AddingToGroup").withLastName("LastName_AddingToGroup").withAddress("Address_AddingToGroup").withEmail("email_addingtogroup@gmail.com").withHomePhone("89995554488"));
     }
 }
